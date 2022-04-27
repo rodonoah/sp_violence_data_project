66137ff154ac3e59e3732b3a449cfcc4d2a63a2f
Rodrigo De Marchi <rodrigodemarchi@Rodrigos-MacBook-Pro.local>
2022-04-26 23:59:56 -0300

    Pop density correlation with violence done

--- extracting_neighborhoods_m2_price.py
+++ extracting_neighborhoods_m2_price.py
@@ -1,17 +1,13 @@
 import pandas as pd
 import time
-import gmplot
-import json
 import requests
 from bs4 import BeautifulSoup
-import pprint
 
 r = requests.get(
     'https://www.agenteimovel.com.br/mercado-imobiliario/a-venda/sp/sao-paulo/#')
 soup = BeautifulSoup(r.text, 'html.parser')
 m2_price = []
 table = soup.find('table', {"class": "bairro"})
-# table_body = table.find('tbody')
 
 rows = table.find_all('tr')
 
@@ -24,8 +20,6 @@ df = pd.DataFrame(m2_price)
 df = df.iloc[1:, :]
 df.set_axis(['bairro', 'alteracao_mensal', 'preco_m2',
             'preco_medio'], axis=1, inplace=True)
-
-# print(df)
 
 t = time.localtime()
 current_time = time.strftime("%b%d%Y%H:%M:%S", t)
